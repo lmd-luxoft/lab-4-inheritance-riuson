325fc6a5554fbe2dec3a1030ef2fa0912ec86c07
Vladimir <riuson@gmail.com>
2020-12-10 21:29:55 +0500

    Add comments to figure types

--- Chess/ChessFigure.cs
+++ Chess/ChessFigure.cs
@@ -16,11 +16,29 @@ namespace Chess
 
         public enum Type
         {
+			/// <summary>
+			/// Ладья.
+			/// </summary>
             ROOK,
+			/// <summary>
+			/// Конь.
+			/// </summary>
             KNIGHT,
+			/// <summary>
+			/// Слон.
+			/// </summary>
             BISHOP,
+			/// <summary>
+			/// Пешка.
+			/// </summary>
             PAWN,
+			/// <summary>
+			/// Король.
+			/// </summary>
             KING,
+			/// <summary>
+			/// Ферзь.
+			/// </summary>
             QUEEN
         }
 
