b3cb28820accd17b89d2bb9c1e465eca2435fb8d
Vladimir <riuson@gmail.com>
2020-12-11 21:09:11 +0500

    Reformat code

--- Chess/ChessFigure.cs
+++ Chess/ChessFigure.cs
@@ -1,128 +1,119 @@
-﻿
-using System;
+﻿using System;
 
 namespace Chess
 {
     public class ChessFigure
     {
-        private FigureType type;
-
-        public ChessFigure(FigureType type, string currentCoord)
-        {
-            this.type = type;
-
-            if (Coordinates.TryParse(currentCoord, out var coordinates))
-            {
-                this.Coordinates = coordinates;
-            }
-            else
-            {
-                throw new FigureException("Нельзя разместить фигуру по неправильным координатам!");
-            }
-        }
-
-		public Coordinates Coordinates { get; }
-
         public enum FigureType
         {
-			/// <summary>
-			/// Ладья.
-			/// </summary>
+            /// <summary>
+            ///     Ладья.
+            /// </summary>
             ROOK,
-			/// <summary>
-			/// Конь.
-			/// </summary>
+
+            /// <summary>
+            ///     Конь.
+            /// </summary>
             KNIGHT,
-			/// <summary>
-			/// Слон.
-			/// </summary>
+
+            /// <summary>
+            ///     Слон.
+            /// </summary>
             BISHOP,
-			/// <summary>
-			/// Пешка.
-			/// </summary>
+
+            /// <summary>
+            ///     Пешка.
+            /// </summary>
             PAWN,
-			/// <summary>
-			/// Король.
-			/// </summary>
+
+            /// <summary>
+            ///     Король.
+            /// </summary>
             KING,
-			/// <summary>
-			/// Ферзь.
-			/// </summary>
+
+            /// <summary>
+            ///     Ферзь.
+            /// </summary>
             QUEEN
         }
 
+        private readonly FigureType _type;
+
+        public ChessFigure(FigureType type, string currentCoord)
+        {
+            _type = type;
+
+            if (Coordinates.TryParse(currentCoord, out var coordinates))
+                Coordinates = coordinates;
+            else
+                throw new FigureException("Нельзя разместить фигуру по неправильным координатам!");
+        }
+
+        public Coordinates Coordinates { get; }
+
         public bool Move(string strNextCoord)
         {
-            if (!Coordinates.TryParse(strNextCoord, out var nextCoord))
+            if (!Coordinates.TryParse(strNextCoord, out var nextCoord)) return false;
+
+            var mvData = new MovementData(Coordinates, nextCoord);
+
+            if (!mvData.IsValid) return false;
+
+            if (_type == FigureType.PAWN)
             {
-                return false;
-            }
-
-            var mvData = new MovementData(this.Coordinates, nextCoord);
-
-            if (!mvData.IsValid)
-            {
-                return false;
-            }
-
-			if (type == FigureType.PAWN)
-			{
-				if (!mvData.IsVertical)
-					return false;
-				if (mvData.DeltaY <= 0)
-					return false;
+                if (!mvData.IsVertical)
+                    return false;
+                if (mvData.DeltaY <= 0)
+                    return false;
                 if (mvData.Start.Y == '2' && mvData.End.Y == '4')
                     return true;
-				if (mvData.DeltaY != 1)
-					return false;
+                if (mvData.DeltaY != 1)
+                    return false;
 
                 return true;
             }
 
-			if (type == FigureType.ROOK)
-			{
-				if ((mvData.DeltaX != 0) && (mvData.DeltaY != 0))
-					return false;
+            if (_type == FigureType.ROOK)
+            {
+                if (mvData.DeltaX != 0 && mvData.DeltaY != 0)
+                    return false;
 
                 return true;
-			}
-			
-            if (type == FigureType.KNIGHT)
-			{
+            }
+
+            if (_type == FigureType.KNIGHT)
+            {
                 if (Math.Abs(mvData.DeltaX) == 1 && Math.Abs(mvData.DeltaY) == 2)
-					return true;
-				if (Math.Abs(mvData.DeltaX) == 2 && Math.Abs(mvData.DeltaY) == 1)
-					return true;
+                    return true;
+                if (Math.Abs(mvData.DeltaX) == 2 && Math.Abs(mvData.DeltaY) == 1)
+                    return true;
 
-				return false;
-			}
-
-			if (type == FigureType.BISHOP)
-			{
-				return mvData.IsDiagonal;
-			}
-
-			if (type == FigureType.KING)
-			{
-				if (Math.Abs(mvData.DeltaX) <= 1 && Math.Abs(mvData.DeltaY) <= 1)
-					return true;
-			
                 return false;
-			}
-			
-            if (type == FigureType.QUEEN)
-			{
-				if (mvData.IsDiagonal)
-					return true;
-				if (mvData.IsVertical)
-					return true;
-				if (mvData.IsHorizontal)
-					return true;
-				
+            }
+
+            if (_type == FigureType.BISHOP) return mvData.IsDiagonal;
+
+            if (_type == FigureType.KING)
+            {
+                if (Math.Abs(mvData.DeltaX) <= 1 && Math.Abs(mvData.DeltaY) <= 1)
+                    return true;
+
                 return false;
-			}
-			
+            }
+
+            if (_type == FigureType.QUEEN)
+            {
+                if (mvData.IsDiagonal)
+                    return true;
+                if (mvData.IsVertical)
+                    return true;
+                if (mvData.IsHorizontal)
+                    return true;
+
+                return false;
+            }
+
             return false;
-		}
+        }
     }
 }
