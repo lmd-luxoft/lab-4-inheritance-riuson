15bd06c99f6248ceda274559dee93fe787b3883a
Vladimir <riuson@gmail.com>
2020-12-11 20:14:13 +0500

    Implement MovementData

--- Chess/MovementData.cs
+++ Chess/MovementData.cs
@@ -1,18 +1,18 @@
-﻿using System;
-
-namespace Chess
+﻿namespace Chess
 {
     public class MovementData
     {
         public MovementData(Coordinates start, Coordinates end)
         {
+            Start = start;
+            End = end;
         }
 
         public Coordinates Start { get; }
         public Coordinates End { get; }
 
-        public int DeltaX => throw new NotImplementedException();
-        public int DeltaY => throw new NotImplementedException();
-        public bool IsValid { get; }
+        public int DeltaX => End.X - Start.X;
+        public int DeltaY => End.Y - Start.Y;
+        public bool IsValid => Start.IsValid && End.IsValid;
     }
 }
